7dfa3f35e3a6d10971c65c8882784bfc3f42bf54
kianenigma <kian.peymani@gmail.com>
2019-09-18 12:19:37 +0200

    First working set of tests.

--- runtime/src/offchaincb.rs
+++ runtime/src/offchaincb.rs
@@ -1,4 +1,4 @@
-//! # Offchain Worker Callback Example 
+//! # Offchain Worker Callback Example
 //!
 //! This is a minimal example module to show case how the runtime can and should
 //! interact with an offchain worker asynchronously.
@@ -16,7 +16,7 @@
 //! first place, we can't allow just anyone to call `pong`. Instead the runtime has a
 //! local list of `authorities`-keys that allowed to evoke `pong`. In this simple example
 //! this list can only be extended via a root call (e.g. `sudo`). In practice more
-//! complex management models and session based key rotations should be conidered, but 
+//! complex management models and session based key rotations should be considered, but
 //! this is out of the scope of this example
 
 // Ensure we're `no_std` when compiling for Wasm. Otherwise our `Vec` and operations
@@ -47,7 +47,7 @@ pub trait Trait: system::Trait  {
 	type Event: From<Event<Self>> + Into<<Self as system::Trait>::Event>
 				+ From<<Self as system::Trait>::Event> + TryInto<Event<Self>>;
 
-	/// A dispatchable call type. We need to define it for the offchain worker to 
+	/// A dispatchable call type. We need to define it for the offchain worker to
 	/// reference the `pong` function it wants to call.
 	type Call: From<Call<Self>>;
 
@@ -76,7 +76,7 @@ decl_event!(
 decl_storage! {
 	trait Store for Module<T: Trait> as OffchainCb {
 		/// The current set of keys that may submit pongs
-		Authorities get(authorities): Vec<T::AccountId>;
+		Authorities get(authorities) config(): Vec<T::AccountId>;
 	}
 }
 
@@ -150,8 +150,8 @@ impl<T: Trait> Module<T> {
 		// and emit the pong and respond by calling `pong` as a transaction
 		// signed with the given key.
 		// This would be the place, where a regular offchain worker would go off
-		// and do its actual thing before reponding async at a later point in time.
-		// 
+		// and do its actual thing before responding async at a later point in time.
+		//
 		// Note, that even though this is run directly on the same block, as we are
 		// creating a new transaction, this will only react _in the following_ block.
 		for e in <system::Module<T>>::events() {
@@ -187,3 +187,218 @@ impl<T: Trait> Module<T> {
 		})
 	}
 }
+
+#[cfg(test)]
+mod tests {
+	use codec::Decode;
+	use std::sync::Arc;
+	use parking_lot::RwLock;
+	use sr_primitives::{
+		Perbill, generic, RuntimeAppPublic,
+		testing::{Header, TestXt, UintAuthorityId},
+		traits::{IdentityLookup, BlakeTwo256, Block, Dispatchable},
+	};
+	use offchain::testing::TestOffchainExt;
+	use primitives::{H256, Blake2Hasher};
+	use support::{construct_runtime, parameter_types, assert_ok};
+	use runtime_io::{with_externalities, TestExternalities};
+	use system;
+	use crate::offchaincb as offchaincb;
+
+	type AccountId = u64;
+	type AccountIndex = u64;
+	type Extrinsic = TestXt<Call, ()>;
+	type SubmitTransaction = system::offchain::TransactionSubmitter<UintAuthorityId, Call, Extrinsic>;
+
+	// TODO: implement this for runtime or call?
+	impl system::offchain::CreateTransaction<TestRuntime, Extrinsic> for Call {
+		type Signature = u64;
+
+		fn create_transaction<F: system::offchain::Signer<AccountId, Self::Signature>>(
+			call: Call,
+			account: AccountId,
+			_index: AccountIndex,
+		) -> Option<(Call, <Extrinsic as sr_primitives::traits::Extrinsic>::SignaturePayload)> {
+			let extra = ();
+			Some((call, (account, extra)))
+		}
+	}
+
+	parameter_types! {
+		pub const BlockHashCount: u64 = 250;
+		pub const MaximumBlockWeight: u32 = 1024;
+		pub const MaximumBlockLength: u32 = 2 * 1024;
+		pub const AvailableBlockRatio: Perbill = Perbill::one();
+	}
+
+	impl system::Trait for TestRuntime {
+		type Origin = Origin;
+		type Index = AccountIndex;
+		type BlockNumber = u64;
+		type Call = Call;
+		type Hash = H256;
+		type Hashing = BlakeTwo256;
+		type AccountId = AccountId;
+		type Lookup = IdentityLookup<Self::AccountId>;
+		type Header = Header;
+		type WeightMultiplierUpdate = ();
+		type Event = Event;
+		type BlockHashCount = BlockHashCount;
+		type MaximumBlockWeight = MaximumBlockWeight;
+		type MaximumBlockLength = MaximumBlockLength;
+		type AvailableBlockRatio = AvailableBlockRatio;
+		type Version = ();
+	}
+
+	impl offchaincb::Trait for TestRuntime {
+		type Event = Event;
+		type Call = Call;
+		type SubmitTransaction = SubmitTransaction;
+		type KeyType = UintAuthorityId;
+	}
+
+	pub type NodeBlock = generic::Block<Header, Extrinsic>;
+	construct_runtime!(
+		pub enum TestRuntime where
+			Block = NodeBlock,
+			NodeBlock = NodeBlock,
+			UncheckedExtrinsic = Extrinsic
+		{
+			System: system::{Module, Call, Event},
+			OffchainCb: offchaincb::{Module, Call, Event<T> ,Config<T>},
+		}
+	);
+
+	pub fn new_test_ext(local_keys: Vec<AccountId>) -> TestExternalities<Blake2Hasher> {
+		let mut t = system::GenesisConfig::default().build_storage::<TestRuntime>().unwrap();
+		// Any node that has local key `49` can submit a pong.
+		offchaincb::GenesisConfig::<TestRuntime> { authorities: vec![49] }
+			.assimilate_storage(&mut t).unwrap();
+		UintAuthorityId::set_all_keys(local_keys);
+		t.into()
+	}
+
+	fn seal_block(n: u64, state: Arc<RwLock<offchain::testing::State>>) -> Option<usize> {
+		assert_eq!(System::block_number(), n);
+		System::set_block_number(n + 1);
+		if let Some(key) = OffchainCb::authority_id() {
+			// run offchain
+			OffchainCb::offchain(&key);
+
+			// if there are any txs submitted to the queue, dispatch them
+			let transactions = &mut state.write().transactions;
+			let count = transactions.len();
+			while let Some(t) = transactions.pop() {
+				let e: Extrinsic = Decode::decode(&mut &*t).unwrap();
+				let (who, _) = e.0.unwrap();
+				let call = e.1;
+				// in reality you would do `e.apply`, but this is a test. we assume we don't care
+				// about validation etc.
+				let _ = call.dispatch(Some(who).into()).unwrap();
+			}
+			Some(count)
+		} else {
+			None
+		}
+	}
+
+	#[test]
+	fn test_setup_works() {
+		// a normal node.
+		with_externalities(&mut new_test_ext(vec![1, 2, 3]), || {
+			assert_eq!(OffchainCb::authorities(), vec![49]);
+			assert_eq!(
+				UintAuthorityId::all(),
+				vec![1u64, 2, 3].into_iter().map(Into::into).collect::<Vec<UintAuthorityId>>()
+			);
+
+			assert_eq!(OffchainCb::is_authority(&1), false);
+			assert_eq!(OffchainCb::is_authority(&2), false);
+			assert_eq!(OffchainCb::is_authority(&3), false);
+			assert_eq!(OffchainCb::is_authority(&49), true);
+
+			assert!(OffchainCb::authority_id().is_none());
+		});
+
+		// an authority node.
+		with_externalities(&mut new_test_ext(vec![2, 49]), || {
+			assert_eq!(OffchainCb::authorities(), vec![49]);
+			assert_eq!(
+				UintAuthorityId::all(),
+				vec![2u64, 49].into_iter().map(Into::into).collect::<Vec<UintAuthorityId>>()
+			);
+			assert!(OffchainCb::authority_id().is_some());
+		});
+	}
+
+	#[test]
+	fn ping_should_work() {
+		with_externalities(&mut new_test_ext(vec![1]), || {
+			assert_ok!(OffchainCb::ping(Origin::signed(1), 1));
+			assert_eq!(
+				System::events()[0].event,
+				Event::offchaincb(offchaincb::RawEvent::Ping(1, 1)),
+			);
+
+		})
+	}
+
+	#[test]
+	fn anyone_can_ping() {
+		with_externalities(&mut new_test_ext(vec![1]), || {
+			// authority
+			assert_ok!(OffchainCb::ping(Origin::signed(1), 1));
+			// normal key
+			assert_ok!(OffchainCb::ping(Origin::signed(2), 4));
+
+			assert_eq!(
+				System::events()[0].event,
+				Event::offchaincb(offchaincb::RawEvent::Ping(1, 1)),
+			);
+
+			assert_eq!(
+				System::events()[1].event,
+				Event::offchaincb(offchaincb::RawEvent::Ping(4, 2)),
+			);
+		})
+	}
+
+	#[test]
+	fn ping_triggers_ack() {
+		let mut ext = new_test_ext(vec![49]);
+		let (offchain, state) = TestOffchainExt::new();
+		ext.set_offchain_externalities(offchain);
+
+		with_externalities(&mut ext, || {
+			// 2 submits a ping. Assume this is an extrinsic from the outer world.
+			assert_ok!(OffchainCb::ping(Origin::signed(2), 1));
+			assert_eq!(
+				System::events()[0].event,
+				Event::offchaincb(offchaincb::RawEvent::Ping(1, 2)),
+			);
+
+			// 49 is an authority (current externality), should be able to call pong.
+			assert!(seal_block(1, state).is_some());
+
+			// which triggers ack
+			assert_eq!(
+				System::events()[1].event,
+				Event::offchaincb(offchaincb::RawEvent::Ack(1, 49)),
+			);
+		})
+	}
+
+	#[test]
+	fn only_authorities_can_pong() {
+		let mut ext = new_test_ext(vec![69]);
+		let (offchain, state) = TestOffchainExt::new();
+		ext.set_offchain_externalities(offchain);
+
+		with_externalities(&mut ext, || {
+			assert_ok!(OffchainCb::ping(Origin::signed(2), 1));
+			// 69 is not an authority.
+			assert!(seal_block(1, state).is_none());
+		})
+	}
+}
+
